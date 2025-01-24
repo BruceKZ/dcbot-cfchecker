27e23988cdacf8fae1ce59c6d146893473f956c6
Zhixiang Dai <105292643+BruceKZ@users.noreply.github.com>
2025-01-24 12:04:08 +0100

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,11 @@
+/**
+ * - BOT_TOKEN：你的Bot令牌（不可泄露）
+ * - DB_PATH: 数据库文件路径
+ * - CLIENT_ID：你在Discord开发者后台拿到的应用ID
+ * - GUILD_ID：如果只想在指定服务器中测试，则填该服务器ID
+ */
+
+const BOT_TOKEN = '';
+const DB_PATH = './verified_users.sqlite';
+const CLIENT_ID = '';
+const GUILD_ID = '';
